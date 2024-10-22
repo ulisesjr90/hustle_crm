4cf834614a2762d70981cf10abdbd48cf2b2a0ba
ulisesjr90 <57239660+ulisesjr90@users.noreply.github.com>
2024-10-21 20:29:34 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,17 +1,9 @@
-<<<<<<< HEAD
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
-};
-=======
+// next.config.js
 const isProd = process.env.NODE_ENV === 'production';
->>>>>>> 3febdd1cd61d4b7c4d11801466c3696ca7ebcd00
 
-module.exports = {
-  assetPrefix: isProd ? '/hustle_crm/' : '',
+const nextConfig = {
   basePath: isProd ? '/hustle_crm' : '',
-  trailingSlash: true,
+  assetPrefix: isProd ? '/hustle_crm/' : '',
 };
+
+module.exports = nextConfig;
