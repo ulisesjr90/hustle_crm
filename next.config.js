3febdd1cd61d4b7c4d11801466c3696ca7ebcd00
ulisesjr90 <57239660+ulisesjr90@users.noreply.github.com>
2024-10-21 20:17:05 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true, 
-};
+const isProd = process.env.NODE_ENV === 'production';
 
-module.exports = nextConfig;
+module.exports = {
+  assetPrefix: isProd ? '/hustle_crm/' : '',
+  basePath: isProd ? '/hustle_crm' : '',
+  trailingSlash: true,
+};
